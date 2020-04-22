e90d01221295fc7bba7febb214ba2f2eb5d5b006
Monish <jmonishram@gmail.com>
2020-04-23 01:32:19 +0530

    WORKING !!! :smiley_cat:

--- js/script.js
+++ js/script.js
@@ -1,63 +1,70 @@
+var counter = 2;
 $(document).ready(function(){
-  
-    var counter = 2;
 
     $("#addButton").click(function () {
 
-  if(counter>10){
-            alert("Only 10 textboxes allow");
-            return false;
-  }
+if(counter>15){
+        alert("Only 15 textboxes allow");
+        return false;
+}
 
-  var newTextBoxDiv = $(document.createElement('div'))
-       .attr("id", 'TextBoxDiv' + counter);
+var newTextBoxDiv = $(document.createElement('div'))
+   .attr("id", 'TextBoxDiv' + counter);
 
-  newTextBoxDiv.after().html('<label>Grade Sub #'+ counter + ' : </label>' +
-        '<input type="text" name="textbox' + counter +
-        '" id="grade' + counter + '" value="" > <label>Credits Sub#'+ counter + ' : </label>' +
-        '<input type="text" name="textbox' + counter +
-        '" id="credit' + counter + '" value="" >');
+newTextBoxDiv.after().html('<label>Grade Sub #'+ counter + ' : </label>' +
+    '<input type="text" name="textbox' + counter +
+    '" id="grade' + counter + '" value="" > <label>Credits Sub#'+ counter + ' : </label>' +
+    '<input type="text" name="textbox' + counter +
+    '" id="credit' + counter + '" value="" >');
 
-  newTextBoxDiv.appendTo("#TextBoxesGroup");
-
-
-  counter++;
-     });
-
-     $("#removeButton").click(function () {
-  if(counter==1){
-          alert("No more fields to remove");
-          return false;
-       }
-
-  counter--;
-
-        $("#TextBoxDiv" + counter).remove();
-
-     });
+newTextBoxDiv.appendTo("#TextBoxesGroup");
 
 
-     function calculate()
-     {
-       var grade = document.getElementById("grade1").value;
-       console.log(grade);
-       var elements = document.querySelectorAll("#gpa input[type=text]")
-      
-       for (var i = 1; i<counter; i++ )
-       {
-         var check = document.getElementById("grade"+i).value;
-         
-         console.log(check);
-         // write your logic here
-       }
-     };
+counter++;
+ });
 
-     $("#getButtonValue").click(function () {
+ $("#removeButton").click(function () {
+if(counter==1){
+      alert("No more fields to remove");
+      return false;
+   }
 
-  var msg = '';
-  for(i=1; i<counter; i++){
-       msg += "\n Textbox #" + i + " : " + $('#credit' + i).val();
-  }
-        alert(msg);
-     });
-  });
+counter--;
+
+    $("#TextBoxDiv" + counter).remove();
+
+ });
+
+
+ 
+
+ $("#getButtonValue").click(function () {
+
+var msg = '';
+for(i=1; i<counter; i++){
+   msg += "\n Textbox #" + i + " : " + $('#credit' + i).val();
+}
+    alert(msg);
+ });
+
+});
+
+function calculate()
+ {
+    var grade = [];
+    var credit = [];
+    var i;
+   for (i = 1; i<counter; i++ )
+   {
+     grade[i] = document.getElementById("grade"+i).value;
+     credit[i] = document.getElementById("credit"+i).value;
+     console.log(grade[i]);
+   }
+
+   for ( i = 1; i<counter; i++ ){
+       
+   }
+
+
+
+ };
