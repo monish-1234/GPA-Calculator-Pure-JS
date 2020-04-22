2c3ea2d93880711534b2672af69d47eb42f71281
Monish <jmonishram@gmail.com>
2020-04-23 03:18:45 +0530

    Working !!!

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 var counter = 2;
+var gpa;
+var cgpa;
 $(document).ready(function(){
 
     $("#addButton").click(function () {
@@ -53,18 +55,55 @@ function calculate()
  {
     var grade = [];
     var credit = [];
+    var g1 = 0;
+    var c1 = 0;
     var i;
+
+    
    for (i = 1; i<counter; i++ )
    {
      grade[i] = document.getElementById("grade"+i).value;
      credit[i] = document.getElementById("credit"+i).value;
-     console.log(grade[i]);
-   }
+    
+    }
 
    for ( i = 1; i<counter; i++ ){
-       
+        if (grade[i]=="O"){
+        grade[i]=10;
+        }
+        if (grade[i]=="A+"){
+                grade[i]=9;
+        }
+        if (grade[i]=="A"){
+                grade[i]=8;
+        }
+        if (grade[i]=="B+"){
+                grade[i]=7;
+        }
+        if (grade[i]=="B"){
+                grade[i]=6;
+        }
+        if (grade[i]=="C"){
+                grade[i]=5;
+        }
+        if (grade[i]=="P"){
+                grade[i]=4;
+        }
+        console.log(grade[i]);
    }
 
+   for (i = 1; i<counter; i++ ){
+    g1+=parseInt(grade[i]*credit[i]);
+    console.log(g1);
+   }
+   for (i = 1; i<counter; i++ ){
+    c1+=parseInt(credit[i]);
+    console.log(c1);
+   }
 
+   
+
+   gpa = parseInt((g1 / c1).toFixed(3));
+    console.log(gpa);
 
  };
