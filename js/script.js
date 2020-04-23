aaeafef05f493447df89be3319a8b461ac3f6abd
Monish <jmonishram@gmail.com>
2020-04-24 01:45:34 +0530

    Final Running Verified

--- js/script.js
+++ js/script.js
@@ -5,41 +5,41 @@ var dumb = 1;
 
 $(document).ready(function(){
 
-    $("#addButton").click(function () {
+ $("#addButton").click(function () {
 
-if(counter>15){
-        alert("Only 15 textboxes allow");
-        return false;
-}
+        if(counter>15){
+                alert("Only 15 textboxes allow");
+                return false;
+        }
 
-var newTextBoxDiv = $(document.createElement('div'))
-   .attr("id", 'TextBoxDiv' + counter);
+        var newTextBoxDiv = $(document.createElement('div'))
+        .attr("id", 'TextBoxDiv' + counter);
 
-newTextBoxDiv.after().html('<br><div id="TextBoxDiv'+counter+'">'+
-                        ' <div class="uk-form-controls">'+
-                  '<label class="uk-form-label">Sub #'+counter+' : </label><select class="uk-select uk-form-width-small" required id="grade'+counter+'">'+
-                    '<option value="" selected hidden disabled >Select Grade</option>'+
-                    '<option value="10">O</option>'+
-                      '<option value="9">A+</option>'+
-                      '<option value="8">A</option>'+
-                      '<option value="7">B+</option>'+
-                      '<option value="6">B</option>'+
-                      '<option value="5">C</option>'+
-                      '<option value="4">P</option>'+                
-                    '</select>'+ 
-                      '&nbsp; &nbsp; &nbsp;'+
-                   '<select class="uk-select uk-form-width-small" required id="credit'+counter+'">'+
-                   '<option value="" selected hidden disabled>Select Credit</option>'+
-                    '<option value="1">1</option>'+
-                    '<option value="2">2</option>'+
-                    '<option value="3">3</option>'+
-                    '<option value="4">4</option>'+
-                    '<option value="5">5</option>'+
-                    '<option value="6">6</option>'+
-                    '<option value="7">7</option>'+                 
-                '</select>'+ 
-              '</div>'+
-              '</div>');
+        newTextBoxDiv.after().html('<br><div id="TextBoxDiv'+counter+'">'+
+                                ' <div class="uk-form-controls">'+
+                        '<label class="uk-form-label">Sub #'+counter+' : </label><select class="uk-select uk-form-width-small" required id="grade'+counter+'">'+
+                        '<option value="" selected hidden disabled >Select Grade</option>'+
+                        '<option value="10">O</option>'+
+                        '<option value="9">A+</option>'+
+                        '<option value="8">A</option>'+
+                        '<option value="7">B+</option>'+
+                        '<option value="6">B</option>'+
+                        '<option value="5">C</option>'+
+                        '<option value="4">P</option>'+                
+                        '</select>'+ 
+                        '&nbsp; &nbsp; &nbsp;'+
+                        '<select class="uk-select uk-form-width-small" required id="credit'+counter+'">'+
+                        '<option value="" selected hidden disabled>Select Credit</option>'+
+                        '<option value="1">1</option>'+
+                        '<option value="2">2</option>'+
+                        '<option value="3">3</option>'+
+                        '<option value="4">4</option>'+
+                        '<option value="5">5</option>'+
+                        '<option value="6">6</option>'+
+                        '<option value="7">7</option>'+                 
+                        '</select>'+ 
+                '</div>'+
+                '</div>');
 
 newTextBoxDiv.appendTo("#TextBoxesGroup");
 
@@ -55,7 +55,10 @@ if(counter==2){
         }
         sound[dumb].play();
         dumb++;
-      alert("No more fields to remove");
+        if (dumb==7){
+                dumb=1;
+        }
+      alert("You cannot remove all the subjects");
       return false;
    }
 
@@ -63,18 +66,6 @@ counter--;
 
     $("#TextBoxDiv" + counter).remove();
 
- });
-
-
- 
-
- $("#getButtonValue").click(function () {
-
-var msg = '';
-for(i=1; i<counter; i++){
-   msg += "\n Textbox #" + i + " : " + $('#credit' + i).val();
-}
-    alert(msg);
  });
 
 });
@@ -103,6 +94,8 @@ function calculate()
    }
 
    gpa = (g1 / c1).toFixed(3);
+   document.getElementById("output").value="Your GPA is "+gpa;
     console.log(gpa);
+    return false
 
  };
